2af5e9090c8f35037e240b6e8f5b4a0711950914
zhanyeye <zhanyeye@qq.com>
2020-07-01 21:27:51 +0800

    完成流式分页

--- pages/mgmt/paper/paper.js
+++ pages/mgmt/paper/paper.js
@@ -4,6 +4,8 @@ Page({
   data: {
     // 后台分页从0开始
     page: 1,
+    pageSize: 30,
+    hasNext: true,
     list: []
   },
   onLoad() {
@@ -11,14 +13,44 @@ Page({
       this.setData({ list: res.data.list });
     });
   },
+  // 顶部下拉刷新
   onPullDownRefresh() {
+    this.data.page = 1;
+    this.data.hasNext = true;
+    listPaper(this.data.page, 6)
+      .then(res => {
+        this.setData({ list: res.data.list });
+        dd.stopPullDownRefresh();
+      })
+      .catch(() => {
+        dd.showToast({
+          type: 'fail',
+          content: '加载失败',
+          duration: 2000
+        });
+      })
+      .finally(() => {
+        dd.stopPullDownRefresh();
+      });
+
     console.log('upper');
   },
+  // 触底上拉加载
   onReachBottom() {
-    // 页面被拉到底部
+    if (this.data.hasNext) {
+      this.data.page++;
+      listPaper(this.data.page, 6).then(res => {
+        console.log(res.data.list);
+        this.setData({ list: this.data.list.concat(res.data.list) });
+        if (res.data.list.length == 0) {
+          this.data.hasNext = false;
+        }
+      });
+    }
+
     console.log('bottom');
   },
   test() {
-    return "1234567890"
+    return '1234567890';
   }
 });
