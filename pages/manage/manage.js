564e01d86e53b9d72f9fc860d385a0f3c82d65e6
zhanyeye <zhanyeye@qq.com>
2020-07-24 18:44:33 +0800

    update perf

--- pages/manage/manage.js
+++ pages/manage/manage.js
@@ -1,44 +1,10 @@
 Page({
   data: {
-    arr: {
-      onItemTap: 'onGridItemTap',
-      list: [
-        {
-          icon: '/image/paper.png',
-          title: '论文管理',
-          entitle: 'Collapse',
-          page: '/pages/manage/paper/paper'
-        },
-        {
-          icon: '/image/apply.png',
-          title: '开发中...',
-          entitle: 'Tag',
-          page: '/page/biz/pages/tag/index'
-        },
-        {
-          icon: '/image/ac.png',
-          title: '开发中...',
-          entitle: 'Tag',
-          page: '/page/biz/pages/tag/index'
-        },
-        {
-          icon: '/image/stat.png',
-          title: '开发中...',
-          entitle: 'Tag',
-          page: '/page/biz/pages/tag/index'
-        },
-        {
-          icon: '/image/dev.png',
-          title: '开发中...',
-          entitle: 'Tag',
-          page: '/page/biz/pages/tag/index'
-        }
-      ]
-    }
+
   },
   onLoad() {},
-  onGridItemTap(e) {
-    const page = this.data.arr.list[e.currentTarget.dataset.index].page;
-    dd.navigateTo({ url: page });
+  tapTab(event) {
+    let tab =  event.target.dataset.tab
+    dd.navigateTo({url: './' + tab + '/' + tab })
   }
 });
